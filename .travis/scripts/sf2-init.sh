5880bf56bebec53031e45dd0e999cfbaf006579f
jubianchi <contact@jubianchi.fr>
2012-08-05 18:07:51 +0200

    Updated test environment

--- .travis/scripts/sf2-init.sh
+++ .travis/scripts/sf2-init.sh
@@ -2,6 +2,7 @@
 
 echo "---> Starting $(tput bold ; tput setaf 2)Symfony2 project initialization$(tput sgr0)"
 
-bin/vendors install
+curl -s https://getcomposer.org/installer | php
+php composer.phar install --dev
 app/console --env=test doctrine:schema:create
 sudo chmod -R 777 app/cache app/logs
