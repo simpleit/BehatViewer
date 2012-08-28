ac05c69ad74332972a254e6890f3f54b42796c06
jubianchi <contact@jubianchi.fr>
2012-08-29 00:32:09 +0200

    Use --prefer-source to avoid an error on boxes

--- .travis/scripts/sf2-init.sh
+++ .travis/scripts/sf2-init.sh
@@ -3,6 +3,6 @@
 echo "---> Starting $(tput bold ; tput setaf 2)Symfony2 project initialization$(tput sgr0)"
 
 curl -s https://getcomposer.org/installer | php
-php composer.phar install --dev
+php composer.phar install --dev --prefer-source
 app/console --env=test doctrine:schema:create
 sudo chmod -R 777 app/cache app/logs
