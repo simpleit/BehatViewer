e2a70dcfdaf4c9d3462dcca81e93160705b9bc0d
jubianchi <contact@jubianchi.fr>
2012-08-29 23:19:16 +0200

    Fix test fixtures

--- src/BehatViewer/BehatViewerBundle/Features/Fixtures/second-project.sql
+++ src/BehatViewer/BehatViewerBundle/Features/Fixtures/second-project.sql
@@ -1,5 +1,5 @@
 INSERT INTO `behatviewer_project` (`id`, `name`, `slug`, `test_command`, `user_id`, `strategy`, `configuration_id`) VALUES
-(2, 'Bar Foo', 'bar-foo', 'app/console bar foo', 'git_local', 2);
+(2, 'Bar Foo', 'bar-foo', 'app/console bar foo', 1, 'git_local', 2);
 
 INSERT INTO `behatviewer_configuration` (`id`, `data`)
 VALUES (2, '{\"repository_path\":\"\\/bar\\/foo\",\"branch\":\"master\"}');
