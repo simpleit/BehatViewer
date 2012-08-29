bbe2b5f276e97303d41e258fd3b9480f00c1179c
jubianchi <contact@jubianchi.fr>
2012-08-29 20:59:33 +0200

    Fix build script (bis)

--- .travis/scripts/sf2-init.sh
+++ .travis/scripts/sf2-init.sh
@@ -2,7 +2,9 @@
 
 echo "---> Starting $(tput bold ; tput setaf 2)Symfony2 project initialization$(tput sgr0)"
 
+sudo chmod -R 777 app/cache app/logs
+
 curl -s https://getcomposer.org/installer | php
 php composer.phar install --dev --prefer-source
+
 app/console --env=test doctrine:schema:create
-sudo chmod -R 777 app/cache app/logs
