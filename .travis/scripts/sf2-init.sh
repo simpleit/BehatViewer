fadc5f18679cb16ae4ce1e3c3a494eb575d8f5c3
jubianchi <contact@jubianchi.fr>
2012-08-29 21:36:43 +0200

    Another test on build script

--- .travis/scripts/sf2-init.sh
+++ .travis/scripts/sf2-init.sh
@@ -2,9 +2,9 @@
 
 echo "---> Starting $(tput bold ; tput setaf 2)Symfony2 project initialization$(tput sgr0)"
 
-sudo chmod -R 777 app/cache app/logs
-
 curl -s https://getcomposer.org/installer | php
 php composer.phar install --dev --prefer-source
+
+sudo chmod -R 777 app/cache app/logs
 
 app/console --env=test doctrine:schema:create
