43d8f69e09755f9144f128b31230e018a71a8678
jubianchi <contact@jubianchi.fr>
2012-08-29 19:24:41 +0200

    Fix build script

--- .travis/scripts/sf2-configure.sh
+++ .travis/scripts/sf2-configure.sh
@@ -13,4 +13,4 @@ fi
 echo "---> Starting $(tput bold ; tput setaf 2)Symfony2 project configuration$(tput sgr0)"
 echo "---> MySQL database name : $(tput bold ; tput setaf 3)$DBNAME$(tput sgr0)"
 
-sed s/%database_name%/$DBNAME/ "$ROOTDIR/app/config/parameters.ini-dist" | sed s/%database_login%/root/ | sed s/%database_password%// | sed s/%secret%/ThisTokenIsNotSoSecret/ > "$ROOTDIR/app/config/parameters.ini"
+sed s/%database_name%/$DBNAME/ "$ROOTDIR/app/config/parameters.ini-dist" | sed s/%database_login%/root/ | sed s/%database_password%// | sed s/%secret%/ThisTokenIsNotSoSecret/ | sed s/%rabbitmq_host%// | sed s/%rabbitmq_user%// | sed s/%rabbitmq_password%// > "$ROOTDIR/app/config/parameters.ini"
