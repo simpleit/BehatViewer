d74b718b8f56b3425d183a8147e78532f80fe949
jubianchi <contact@jubianchi.fr>
2012-09-03 21:22:02 +0200

    Fix test fixtures

--- src/BehatViewer/BehatViewerBundle/Features/Fixtures/admin.sql
+++ src/BehatViewer/BehatViewerBundle/Features/Fixtures/admin.sql
@@ -1,2 +1,6 @@
-INSERT INTO `behatviewer_user` (`username`, `salt`, `password`, `email`, `is_active`)
-VALUES ('admin', '25a471df497cf7c89c1d1f38c889fb2c', '5f06047b0c4cf7473efc0782268d716dc038333c', 'admin@foo.bar', 1);
+INSERT INTO behatviewer_role(`id`, `name`, `role`) VALUES(2, 'Admin', 'ROLE_admin');
+
+INSERT INTO `behatviewer_user` (`id`, `username`, `salt`, `password`, `email`, `is_active`)
+VALUES (2, 'admin', '25a471df497cf7c89c1d1f38c889fb2c', '5f06047b0c4cf7473efc0782268d716dc038333c', 'admin@foo.bar', 1);
+
+INSERT INTO behatviewer_user_role VALUES(2, 2);
