daedb0306c169e7d2cf5502efaa59988ae1dc044
jubianchi <contact@jubianchi.fr>
2012-09-08 16:12:15 +0200

    Fix fixtures

--- src/BehatViewer/BehatViewerBundle/Features/Fixtures/single-project.sql
+++ src/BehatViewer/BehatViewerBundle/Features/Fixtures/single-project.sql
@@ -1,5 +1,5 @@
-INSERT INTO `behatviewer_project` (`id`, `name`, `slug`, `test_command`, `user_id`, `strategy`, `configuration_id`) VALUES
-(1, 'Foo Bar', 'foo-bar', 'app/console foo bar', 1, 'git_local', 1);
+INSERT INTO `behatviewer_project` (`id`, `name`, `slug`, `test_command`, `user_id`, `configuration_id`) VALUES
+(1, 'Foo Bar', 'foo-bar', 'app/console foo bar', 1, 1);
 
 INSERT INTO `behatviewer_configuration` (`id`, `data`)
 VALUES (1, '{\"repository_path\":\"\\/foo\\/bar\",\"branch\":\"master\"}');
