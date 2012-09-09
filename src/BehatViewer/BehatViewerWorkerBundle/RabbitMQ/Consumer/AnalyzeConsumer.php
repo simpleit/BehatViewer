c6e7d78733e5ef04e92420e46320d74ecce7726b
jubianchi <contact@jubianchi.fr>
2012-09-09 22:33:08 +0200

    Fix analyze consumer payload handling

--- src/BehatViewer/BehatViewerWorkerBundle/RabbitMQ/Consumer/AnalyzeConsumer.php
+++ src/BehatViewer/BehatViewerWorkerBundle/RabbitMQ/Consumer/AnalyzeConsumer.php
@@ -13,7 +13,7 @@ class AnalyzeConsumer extends Consumer
 		$repository = $this->container->get('doctrine')->getRepository('BehatViewerBundle:Project');
 		$project = $repository->findOneBySlug($options['project']);
 
-		$data = json_decode(file_get_contents($options['payload']), true);
+		$data = json_decode($options['payload'], true);
 		$analyzer = $this->getContainer()->get('behat_viewer.analyzer');
 		$analyzer->analyze($project, $data);
 
