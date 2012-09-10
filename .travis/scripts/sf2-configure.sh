3c41f1c86766521d5e744495b8db709aa8d437f0
jubianchi <contact@jubianchi.fr>
2012-09-10 23:58:17 +0200

    Trying to fix config script

--- .travis/scripts/sf2-configure.sh
+++ .travis/scripts/sf2-configure.sh
@@ -13,4 +13,12 @@ fi
 echo "---> Starting $(tput bold ; tput setaf 2)Symfony2 project configuration$(tput sgr0)"
 echo "---> MySQL database name : $(tput bold ; tput setaf 3)$DBNAME$(tput sgr0)"
 
-sed s/%database_name%/$DBNAME/ "$ROOTDIR/app/config/parameters.ini-dist" | sed s/%database_login%/root/ | sed s/%database_password%// | sed s/%secret%/ThisTokenIsNotSoSecret/ | sed s/%rabbitmq_host%// | sed s/%rabbitmq_user%// | sed s/%rabbitmq_password%// > "$ROOTDIR/app/config/parameters.ini"
+sed s/%database_name%/$DBNAME/ "$ROOTDIR/app/config/parameters.ini-dist" \
+    | sed s/%database_login%/root/ | sed s/%database_password%// \
+    | sed s/%secret%/ThisTokenIsNotSoSecret/ \
+    | sed s/%rabbitmq_host%// \
+    | sed s/%rabbitmq_user%// \
+    | sed s/%rabbitmq_password%// \
+    | sed s/%piwik_enabled%/false/ \
+    | sed s/%piwik_host%// \
+    | sed s/%piwik_site_id%// > "$ROOTDIR/app/config/parameters.ini"
