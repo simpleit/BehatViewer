19038139435f86541b161ae6ac8f52b845a4b165
jubianchi <contact@jubianchi.fr>
2012-09-16 21:49:09 +0200

    Trying to fix build command

--- src/BehatViewer/BehatViewerWorkerBundle/Command/BuildCommand.php
+++ src/BehatViewer/BehatViewerWorkerBundle/Command/BuildCommand.php
@@ -58,14 +58,20 @@ class BuildCommand extends ProjectCommand
 
 		$pusherOutput->setChannel('job-' . $job->getId());
 
-        $status = $this->getContainer()->get('behat_viewer.builder')->build(
-			$this->getProject()->getStrategy(),
-			$output
-		);
+		try {
+			$status = $this->getContainer()->get('behat_viewer.builder')->build(
+				$this->getProject()->getStrategy(),
+				$output
+			);
 
-		$result = $status === 0
-			? \BehatViewer\BehatViewerWorkerBundle\DBAL\Type\EnumJobStatusType::TYPE_SUCCESS
-			: \BehatViewer\BehatViewerWorkerBundle\DBAL\Type\EnumJobStatusType::TYPE_FAILED;
+			$result = $status === 0
+				? \BehatViewer\BehatViewerWorkerBundle\DBAL\Type\EnumJobStatusType::TYPE_SUCCESS
+				: \BehatViewer\BehatViewerWorkerBundle\DBAL\Type\EnumJobStatusType::TYPE_FAILED;
+
+		} catch(\Exception $exception) {
+			$result = \BehatViewer\BehatViewerWorkerBundle\DBAL\Type\EnumJobStatusType::TYPE_FAILED;
+			$status = $exception->getCode() ?: 1;
+		}
 
 		$job->setStatus($result);
 		$this->getDoctrine()->getManager()->persist($job);
